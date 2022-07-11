9d9be201c492d4744849ef5cb40250d3fc7642af
masterkai <kai0919@hotmail.com>
2022-07-11 20:15:02 +0800

    add: nav

---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit, OnDestroy {
+
+  constructor(public modalService: ModalService) { }
+
+  ngOnInit(): void {
+    this.modalService.register('auth')
+  }
+
+  ngOnDestroy() {
+    this.modalService.unregister('auth')
+  }
+
+}
