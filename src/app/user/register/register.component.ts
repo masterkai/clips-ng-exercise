664f2250d08902ac4f4c9ced18a4e2e3b82623b0
masterkai <kai0919@hotmail.com>
2022-08-05 07:38:33 +0800

    add register

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -20,7 +20,7 @@ export class RegisterComponent {
     Validators.min(18),
     Validators.max(120)
   ])
-  password = new  FormControl('', [
+  password = new FormControl('', [
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
   ])
@@ -29,8 +29,8 @@ export class RegisterComponent {
   ])
   phoneNumber = new FormControl('', [
     Validators.required,
-    Validators.minLength(13),
-    Validators.maxLength(13)
+    Validators.minLength(16),
+    Validators.maxLength(16)
   ])
   showAlert = false
   alertMsg = 'Please wait! Your account is being created.'
